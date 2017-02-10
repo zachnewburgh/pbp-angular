c820e237b3872ea4a897af7b0342642f109127f4
Newburgh <znewburg@na.blkint.com>
2017-02-10 16:19:27 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Angular - Zach Newburgh
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html'
+})
+export class AppComponent  { 
+	title = 'Customer App';
+	name = 'Ward';
+	wardsColor = 'blue';
+
+	changeSuitColor() {
+		this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+	}
+}
